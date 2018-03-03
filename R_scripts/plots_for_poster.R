2f85eb66ae16961f4e7154628c70be7da1d6477e
Rex Thompson <RexS.Thompson@gmail.com>
2018-03-02 17:35:35 -0800

    added raster plot creation script for poster

---
+++ R_scripts/plots_for_poster.R
@@ -0,0 +1,40 @@
+library(raster)
+library(rgdal)
+
+setwd("~/Desktop/HydroSatML")
+
+#### DATA IMPORT
+
+# OD satellite swath raster
+od <- raster("~/Downloads/Od_ndre.630.2016.asc")
+
+# OD farm shape
+shpfile_path <- "./data/TierII_wshed_boundaries/Od_wshed/"
+shpfile <- "wshed_odberg"
+shape <- readOGR(shpfile_path, shpfile)
+
+#### MASKS
+
+# bare soil cutoff
+od[od < 0.48] <- NA
+
+# NW corner cutout
+od[cellFromRowColCombine(od, 1:120, 1:40)] <- NA
+od[cellFromRowColCombine(od, 1:90, 1:100)] <- NA
+od[cellFromRowColCombine(od, 1:55, 1:160)] <- NA
+
+# SE corner cutout
+od[cellFromRowColCombine(od, 300:348, 170:284)] <- NA
+od[cellFromRowColCombine(od, 250:348, 200:284)] <- NA
+od[cellFromRowColCombine(od, 200:348, 240:284)] <- NA
+
+plot(od, colNA="gray50")
+plot(shape, lwd=4, border = "red", add=TRUE)
+
+#### SUBSET PLOT
+
+# Subset shape
+masked <- mask(od, shape)
+# plot(masked)
+trimmed <- trim(masked)
+plot(trimmed, colNA="gray50")
