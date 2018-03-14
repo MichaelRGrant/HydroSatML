4e96b7cdde81e7d4a4cdb44636941efc184568a0
Samir Patel <sdpatel@uw.edu>
2018-03-14 09:38:34 -0700

    Code cleanup complete

--- scripts/2_utilities/visualization_scripts/box_plots_error.R
+++ scripts/2_utilities/visualization_scripts/box_plots_error.R
@@ -1,44 +1,66 @@
+# This script creates a box plot comparing Mean Absolute Errors
+#  for comparing three of ML models and a SMR (physical) model.
+#  Created for Capstone Final Poster.
+
+# Setting working directory
 setwd("~/MS_Data_Science/Capstone/HydroSatML/")
 
+# Importing Libraries
 library(ggplot2)
 library(readr)
 library(dplyr)
 
-NN <- data.frame(read_csv(file = './data/results_data/results_df_NN.csv')[,-1])
-RR <- data.frame(read_csv(file = './data/results_data/results_df_lin_RR.csv')[,-1])
-XG <- data.frame(read_csv(file = './data/results_data/xg_mae_df_results.csv')[,-1])
-SMR <- data.frame(read_csv(file = './data/results_data/merge_sm_error_results.csv'))
+# Import CSVs for each of the models
+NN <- data.frame(read_csv(file = "./data/results_data/results_df_NN.csv")[, -1])
+RR <- data.frame(read_csv(file = "./data/results_data/results_df_lin_RR.csv")[, -1])
+XG <- data.frame(read_csv(file = "./data/results_data/xg_mae_df_results.csv")[, -1])
+SMR <- data.frame(read_csv(file = "./data/results_data/merge_sm_error_results.csv"))
 
+# Organize data for plotting
 summary_tbl <- data.frame(SMR %>%
-    group_by(date) %>%
-    summarise('MAE' = mean(error, na.rm=T)))
+                            group_by(date) %>%
+                            summarise("MAE" = mean(error, na.rm = T)))
 
 
-SMR_error <- data.frame('MAE' = summary_tbl$MAE)
+SMR_error <- data.frame("MAE" = summary_tbl$MAE)
 
-NN$model <- 'Neural Network'
-names(NN)[1] <- 'MAE'
-RR$model <- 'Ridge Regression'
-names(RR)[1] <- 'MAE'
-XG$model <- 'XGBoost'
-names(XG)[1] <- 'MAE'
-SMR_error$model <- 'SMR'
-tidy_error <- rbind(NN, RR, XG, SMR_error)
+NN$model <- "Neural\nNetwork"
+names(NN)[1] <- "MAE"
+RR$model <- "Ridge\nRegression"
+names(RR)[1] <- "MAE"
+XG$model <- "XGBoost"
+names(XG)[1] <- "MAE"
+SMR_error$model <- "SMR"
+tidy_error <- rbind(RR, XG, NN, SMR_error)
 
-tidy_error$MAE <- sapply(tidy_error$MAE, function(x) ifelse(x=='NaN', NA, x))
+tidy_error$model <- factor(tidy_error$model, levels = c("Ridge\nRegression",
+                                                        "XGBoost",
+                                                        "Neural\nNetwork",
+                                                        "SMR"))
+
+
+tidy_error$MAE <- sapply(tidy_error$MAE, function(x) ifelse(x == "NaN", NA, x))
 tidy_error <- na.omit(tidy_error)
 
-boxplot <- ggplot(tidy_error, aes(x=model, y=MAE)) +
-    geom_boxplot(aes(fill = model)) +
-    xlab('') +
-    ylab('Mean Absolute Error') +
-    ggtitle('Comparing Error Between Models') +
-    labs(fill='Model') +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 25),
-          axis.text.y = element_text(size = 25,
-                                     margin = margin(0,0,0,15)),
-          axis.title.x = element_text(size = 35,
-                                      margin = margin(0,0,0,10)),
-          title = element_text(size = 45),
-          legend.position = 'none')
+# Use GGPlot for Boxplot creation
+boxplot <- ggplot(tidy_error, aes(x = model, y = MAE)) +
+  coord_fixed(ratio = 40) +
+  geom_boxplot(aes(fill = model)) +
+  xlab("") +
+  ylab("Mean Absolute Error") +
+  labs(fill = "Model") +
+  theme(axis.text = element_text(size = 50),
+        title = element_text(size = 50),
+        axis.title.y = element_text(margin = margin(t = 40, r = 40, b = 40, l = 40)),
+        legend.position = "none")
+
+
 boxplot
+
+# Save Image Locally
+png("C:/Users/Samir Patel/Desktop/Data 590 - Capstone I/HydroSatML/poster/figures/Figure_3.png", 1480, 1480, pointsize = 36)
+
+par(bty = "n")
+
+plot(boxplot)
+dev.off()
